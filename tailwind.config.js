1461e8ed93cf02ae6d39f9b0b3c0e6d38eabb83b
Leon Hartmann <Leon.hartmann@sportfive.com>
2025-04-27 21:35:37 +0200

    Update package.json and configuration files: upgrade eslint and autoprefixer, add postcss, and enhance tailwind configuration with dark mode support.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
+  darkMode: 'class',
   plugins: [],
 } 
